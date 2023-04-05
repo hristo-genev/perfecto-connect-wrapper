b83b6a0d8e2df1b382d364918c82e54295cd290a
hristo.genev <hristo.genev@gmail.com>
2023-04-05 15:30:14 +0300

    working on mac

--- perfectoconnect.py
+++ perfectoconnect.py
@@ -1,32 +1,40 @@
 import os
 import sys
-import json
+import perfecto_token_provider
 
 if len(sys.argv) < 2:
-  print ("No cloud name provided. Exiting")
-  sys.exit(0)
+    print("No cloud name provided. Exiting")
+    sys.exit(0)
 
-cloud = sys.argv[1]
-hostname = cloud if cloud.endswith(".perfectomobile.com") else cloud + ".perfectomobile.com"
-authFilePath = os.getenv("TOKEN_STORAGE")
-if authFilePath is None:
-  print ("Missing TOKEN_STORAGE variable pointing to the location of the tokens file")
-  sys.exit(0)
-  
-token = ""
+domain = ".perfectomobile.com"
+if sys.argv[1].endswith(domain):
+    cloud = sys.argv[1].replace(domain, "")
+    hostname = sys.argv[1]
+else:
+    hostname = sys.argv[1] + domain
+    cloud = sys.argv[1]
+
 pConnectDownloadUrl = "https://downloads.connect.perfectomobile.com/clients/Perfecto-Connect-windows64.zip"
+pConnectDownloadUrlMac = "https://downloads.connect.perfectomobile.com/clients/Perfecto-Connect-mac.tar"
 
-try:
-  token = json.load(open(authFilePath))["tokens"][cloud]       
-  print ("Found token for %s" % cloud)
-except KeyError as er:
-  print ("Token not found for %s" % cloud)
-  sys.exit(0)
-except Exception:
-  print (sys.exc_info())
-  sys.exit(0)
-  
-command = "perfectoconnect64.exe start -c %s -s %s -v" % (hostname, token)
-print ("Executting %s " % command)
+executable_name = "perfectoconnect"
+if sys.platform.startswith("Windows"):
+    executable_name = "perfectoconnect64.exe"
+
+perfecto_connect_bin = os.environ.get("PERFECTOCONNECT_BIN")
+if perfecto_connect_bin is None:
+    perfecto_connect_bin = executable_name
+
+hostname = cloud if cloud.endswith(".perfectomobile.com") else cloud + ".perfectomobile.com"
+token = perfecto_token_provider.perfecto_token_provider.get_token_for_cloud(cloud)
+if token is None:
+    print("Found token for %s" % cloud)
+    sys.exit(0)
+
+command = "%s start -c %s -s %s -v" % (executable_name, hostname, token)
+if len(sys.argv) == 3 and sys.argv[2] == 'doctor':
+    command = "%s doctor -c %s -s %s -v" % (executable_name, hostname, token)
+
+print("Executing %s " % command)
 
 os.system(command)
